e48ada784d3f9b96ce88431fe539f2a50daf0b68
Matias Sallent <matias.sallent@mediamonks.com>
2020-04-20 12:48:36 -0300

    Remove comments

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,3 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
 import { wrapPageElement as wrap } from './src/utils/wrapPageElement';
 
 export const wrapPageElement = wrap;
