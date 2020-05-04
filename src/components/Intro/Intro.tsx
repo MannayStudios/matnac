3cc9ff1a1260d890acaf1f7f2be58e56cdf52404
Matias Sallent <matias.sallent@mediamonks.com>
2020-05-04 02:12:41 -0300

    Set initial values to prevent flashing

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -225,7 +225,8 @@ export const Intro: React.FC = () => (
         </linearGradient>
       </defs>
       <motion.g
-        animate={{ opacity: [0, 1] }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
         transition={{ duration: 0.5, ease: 'easeInOut', delay: 2.7 }}
       >
         <path fill="url(#c)" d="M109.9 3.4v35c0 8-6.5 14.4-14.6 14.4V3.4h14.6z" />
@@ -262,14 +263,17 @@ export const Intro: React.FC = () => (
 	c-0.1,0.1-0.2,0.2-0.3,0.4c-3.1,3.8-7.8,6.2-13.1,6.2H3.1v16.5c4.6,0,9-1.1,13-3.1s7.5-4.8,10.2-8.5l1.8-2.4l10.2-13
 	c0.1-0.1,0.2-0.2,0.3-0.4v10.9V37v16.3v0.1c4.6,0,9-1.1,13-3.1s7.5-4.8,10.2-8.5l1.8-2.4l10.2-13c0.1-0.1,0.2-0.2,0.3-0.4v12.3
   c0,8,6.5,14.4,14.6,14.4V19.9V3.4C84.1,3.4,79.7,4.5,75.7,6.5z"
-        animate={{ pathLength: [0, 1], opacity: [1, 0] }}
+        strokeDasharray="0 1"
+        initial={{ pathLength: 0, opacity: 1 }}
+        animate={{ pathLength: 1, opacity: 0 }}
         transition={{
           pathLength: { duration: 2, ease: 'easeInOut' },
           opacity: { duration: 1, ease: 'easeInOut', delay: 1.75 },
         }}
       />
       <motion.g
-        animate={{ opacity: [0, 1], x: [500, 0] }}
+        initial={{ opacity: 0, x: 500 }}
+        animate={{ opacity: 1, x: 0 }}
         transition={{
           opacity: { duration: 1.5, ease: 'easeInOut', delay: 1.85 },
           x: { duration: 0.5, ease: 'easeInOut', delay: 2.55 },
