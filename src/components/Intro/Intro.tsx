f4576d329eb0054166d0562835d4b131d9214faf
Matias Sallent <matias.sallent@mediamonks.com>
2020-05-04 02:18:26 -0300

    Move stroke-dasharray to styled

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -18,6 +18,7 @@ const TracedPath = styled(motion.path)`
   fill: transparent;
   stroke: #fff;
   stroke-width: 1.3;
+  stroke-dasharray: '0 1';
   transform: translateX(calc(50% - 3.75rem));
 
   @media (min-width: 768px) {
@@ -263,7 +264,6 @@ export const Intro: React.FC = () => (
 	c-0.1,0.1-0.2,0.2-0.3,0.4c-3.1,3.8-7.8,6.2-13.1,6.2H3.1v16.5c4.6,0,9-1.1,13-3.1s7.5-4.8,10.2-8.5l1.8-2.4l10.2-13
 	c0.1-0.1,0.2-0.2,0.3-0.4v10.9V37v16.3v0.1c4.6,0,9-1.1,13-3.1s7.5-4.8,10.2-8.5l1.8-2.4l10.2-13c0.1-0.1,0.2-0.2,0.3-0.4v12.3
   c0,8,6.5,14.4,14.6,14.4V19.9V3.4C84.1,3.4,79.7,4.5,75.7,6.5z"
-        strokeDasharray="0 1"
         initial={{ pathLength: 0, opacity: 1 }}
         animate={{ pathLength: 1, opacity: 0 }}
         transition={{
